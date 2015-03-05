25582e71ff5d2cd8b4080526b4637dc158a1d3d9
meredithfranklin <meredith.franklin@usc.edu>
2015-03-05 10:04:28 -0800

    new modeling file

---
+++ MISR modeling.R
@@ -0,0 +1,35 @@
+##############################################
+# MISR 2008-2009 4km data analysis
+# February, March 2015
+# Meredith Franklin
+##############################################
+
+# Matched MISR-AQS data
+MISR.AQS<-read.csv("/Users/mf/Documents/MISR/Data/MISR.AQS.csv")
+
+# Visualizations and regressions
+
+# MISR AOD and AQS PM2.5
+plot(MISR.AQS$AOD,MISR.AQS$Daily.Mean.PM2.5.Concentration,xlab='MISR AOD',ylab='AQS PM2.5')
+abline(lm(Daily.Mean.PM2.5.Concentration~AOD, data=MISR.AQS), col="red")
+
+cor(MISR.AQS$AOD,MISR.AQS$Daily.Mean.PM2.5.Concentration)
+lm.MISR.AOD<-lm(Daily.Mean.PM2.5.Concentration~AOD, data=MISR.AQS)
+
+# MISR AOD Small and AQS PM2.5
+plot(MISR.AQS$AODsmall,MISR.AQS$Daily.Mean.PM2.5.Concentration,xlab='MISR AOD',ylab='AQS PM2.5')
+abline(lm(Daily.Mean.PM2.5.Concentration~AODsmall, data=MISR.AQS), col="red")
+cor(MISR.AQS$AODsmall,MISR.AQS$Daily.Mean.PM2.5.Concentration)
+lm.MISR.AOD.small<-lm(Daily.Mean.PM2.5.Concentration~AODsmall, data=MISR.AQS)
+
+# MISR AOD Medium and AQS PM2.5
+plot(MISR.AQS$AODmed,MISR.AQS$Daily.Mean.PM2.5.Concentration,xlab='MISR AOD',ylab='AQS PM2.5')
+abline(lm(Daily.Mean.PM2.5.Concentration~AODmed, data=MISR.AQS), col="red")
+cor(MISR.AQS$AODmed,MISR.AQS$Daily.Mean.PM2.5.Concentration)
+lm.MISR.AOD.med<-lm(Daily.Mean.PM2.5.Concentration~AODmed, data=MISR.AQS)
+
+# MISR AOD Large and AQS PM2.5
+plot(MISR.AQS$AODlarge,MISR.AQS$Daily.Mean.PM2.5.Concentration,xlab='MISR AOD',ylab='AQS PM2.5')
+abline(lm(Daily.Mean.PM2.5.Concentration~AODlarge, data=MISR.AQS), col="red")
+cor(MISR.AQS$AODlarge,MISR.AQS$Daily.Mean.PM2.5.Concentration)
+lm.MISR.AOD.large<-lm(Daily.Mean.PM2.5.Concentration~AODlarge, data=MISR.AQS)
